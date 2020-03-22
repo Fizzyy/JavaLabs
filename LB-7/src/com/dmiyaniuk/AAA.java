28a066274dc858a1f77b826257e33f530944706f
Dzmitry.Yaniuk <Dzmitry.Yaniuk@coxautoinc.com>
2020-03-22 16:41:15 +0300

    Add 7 lab with JAR-file

---
+++ LB-7/src/com/dmiyaniuk/AAA.java
@@ -0,0 +1,13 @@
+package com.dmiyaniuk;
+
+import jarpack.FirstClass;
+
+public class AAA extends FirstClass {
+    public AAA() {
+        super();
+    }
+
+    public void getMessage() {
+        System.out.println("Message from AAA");
+    }
+}
---
+++ LB-7/src/com/dmiyaniuk/MMM.java
@@ -0,0 +1,18 @@
+package com.dmiyaniuk;
+
+import jarpack.FirstClass;
+import jarpack.jarpack2.SecondClass;
+import jarpack.jarpack2.jarpack3.ThirdClass;
+import jarpack.jarpack2.jarpack3.jarpack4.FourhClass;
+
+public class MMM {
+    public static void main(String[] args) {
+        AAA aaa = new AAA();
+        aaa.getMessage();
+
+        FirstClass firstClass = new FirstClass();
+        SecondClass secondClass = new SecondClass();
+        ThirdClass thirdClass = new ThirdClass();
+        FourhClass fourhClass = new FourhClass();
+    }
+}
